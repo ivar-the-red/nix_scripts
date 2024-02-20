90f90f7c0d3de89ee8b229e8584b89b16d23a95a
ivar-the-red <ivar_the_red@protonmail.com>
2024-02-20 18:47:50 +0000

    add pv to install list
    Signed-off-by: ivar-the-red <ivar_the_red@protonmail.com>

--- rpi-scripts/setup-rpi.sh
+++ rpi-scripts/setup-rpi.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt autoremove
 
 sudo apt install git -y
-
+sudo apt install pv -y
 sudo apt install python3-pip -y
 
 sudo apt install python3-gpiozero -y
