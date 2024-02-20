16082408713c2cdcdaa77e6a542b070eb93b7297
ivar-the-red <ivar_the_red@protonmail.com>
2024-02-20 10:31:32 +0000

    Added initial version of set up script
    Signed-off-by: ivar-the-red <ivar_the_red@protonmail.com>

---
+++ rpi-scripts/setup-rpi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove
+
+sudo apt install git -y
+
+sudo apt install python3-pip -y
+
+sudo apt install python3-gpiozero -y
+sudo apt install python3-matplotlib -y
+
